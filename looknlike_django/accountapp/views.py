d43f7994beb8836dc2107b3908fbca6cb5e3fb95
Soun Sean Kim <sukim2406@gmail.com>
2022-05-07 18:15:20 +0900

    looknlike_django: Basic account app done ::::: Daily Commit Project 05-07-2022

--- looknlike_django/accountapp/views.py
+++ looknlike_django/accountapp/views.py
@@ -1,15 +1,22 @@
 
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
+
+from .decorators import account_ownership_required
+from .forms import AccountUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
-def hello_world(request):
+has_onwership = [account_ownership_required, login_required]
 
+@login_required
+def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
@@ -30,3 +37,27 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    template_name = 'accountapp/detail.html'
+    context_object_name = 'target_user'
+
+
+@method_decorator(has_onwership, 'get')
+@method_decorator(has_onwership, 'post')
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+    context_object_name = 'target_user'
+
+@method_decorator(has_onwership, 'get')
+@method_decorator(has_onwership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
+    context_object_name = 'target_user'

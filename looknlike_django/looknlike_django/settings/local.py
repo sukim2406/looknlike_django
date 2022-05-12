ed46feb915c881b31a6a947cb5557736fca7d247
Soun Sean Kim <sukim2406@gmail.com>
2022-05-12 12:19:37 +0900

    looknlike_django : setting file all set ::::: daily commit project 05-12-2022

---
+++ looknlike_django/looknlike_django/settings/local.py
@@ -0,0 +1,22 @@
+from .base import *
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
